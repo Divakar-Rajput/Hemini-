d0c31fbcfa0616db5c4c9622914f5daa6cabe583
DIVAKAR RAJPUT <114133300+Divakar-Rajput@users.noreply.github.com>
2025-03-11 12:04:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let userMessage = null;
 let isResponseGenerating = false;
 
 // API configuration
-const API_KEY = "AIzaSyCehJYN2fdBJ6oTbi01xtQxT-vlrbWX8MA"; // Your API key here
+const API_KEY = "AIzaSyCdpJ2ZSxNDCE7-VhOY_8SqQbG1H2srGOg"; // Your API key here
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 // Load theme and chat data from local storage on page load
